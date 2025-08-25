eed355f7be108bee65d71c2cc118ad4dee8949f8
Mithungowda18 <mithunkumi001122@gmail.com>
2025-08-25 11:24:50 +0530

    Add files via upload

---
+++ OnlineFoodOrderApplication.java
@@ -0,0 +1,17 @@
+package orderapp.Online_Food_Order_Application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@EnableCaching
+@EnableScheduling
+@SpringBootApplication
+public class OnlineFoodOrderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineFoodOrderApplication.class, args);
+	}
+
+}
